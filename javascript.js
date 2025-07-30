2710100207c1670a0dad3166b913158143caee17
sina3brahimi <sinaebrahimidds@gmail.com>
2025-07-30 17:09:42 -0400

    feat: add computer and human choice functions for Rock Paper Scissors
    - Implemented getComputerChoice() to randomly return rock, paper, or scissors
    - Implemented getHumanChoice() using prompt() for user input
    - Added logic for mapping random numbers (0,1,2) to choices
    - Prepared functions for testing game rounds

--- javascript.js
+++ javascript.js
@@ -2,4 +2,21 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
 
-console.log("Hello World");
+function getComputerChoice() {
+  let value = Math.floor(Math.random() * 3);
+
+  if (value === 0) {
+    return "rock";
+  } else if (value === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+  
+}
+
+function getHumanChoice() {
+  let choice = prompt("Choose rock, paper or scissors:");
+  return choice.toLowerCase();
+    
+}
